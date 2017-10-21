5ee648f20c390ab7c9afefe4a65c771c396c81ba
mat1th <github@dolstra.me>
2017-10-21 12:28:03 +0200

    add open sans;

--- resources/data/generalText.js
+++ resources/data/generalText.js
@@ -5,6 +5,41 @@ var generalText = {
   'main': {
     'title': 'Welcome'
     // 'details': 'Bekijk details'
-  }
+  },
+  themes: [{
+    number: '#1',
+    title: 'A dive into time',
+    sessions: [{
+      title: 'The time',
+      kind: 'video',
+      content: [{
+        // videoUrl: require(''),
+        resources: [{
+            url: 'https://youtube.com/',
+            title: 'youtube'
+          },
+          {
+            url: 'https://youtube.com/',
+            title: 'youtube2'
+          }
+        ]
+      }]
+    },{
+      title: 'Into time',
+      kind: 'sound',
+      content: [{
+        // soundUrl: require(''),
+        resources: [{
+            url: 'https://youtube.com/',
+            title: 'youtube'
+          },
+          {
+            url: 'https://youtube.com/',
+            title: 'youtube2'
+          }
+        ]
+      }]
+    }]
+  }]
 };
 export default generalText;
